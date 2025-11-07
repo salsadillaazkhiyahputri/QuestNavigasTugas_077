408ccb40e7c278136fada04f955ba45e37d7c3a5
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-07 16:12:56 +0700

    mrnambahkan Composable DataApp dengan NavController

--- app/src/main/java/com/example/questnavigastugas/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas/Navigasi.kt
@@ -1,8 +1,23 @@
 package com.example.questnavigastugas
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
 enum class Navigasi{
     Home,
     Formulirku,
     Detail
 }
+
+@Composable
+fun DataApp(
+    navController: NavHostController  = rememberNavController(),
+    modifier: Modifier
+){
+
+}
+
 
