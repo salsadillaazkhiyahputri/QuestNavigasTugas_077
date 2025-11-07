76d3aed77ebbad1edeee38898238a97ffb3b8a93
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-07 16:18:26 +0700

    Setup NavHost dengan FormIsian dan navigasi ke Detail

--- app/src/main/java/com/example/questnavigastugas/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas/Navigasi.kt
@@ -1,9 +1,13 @@
 package com.example.questnavigastugas
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi{
@@ -17,6 +21,22 @@ fun DataApp(
     navController: NavHostController  = rememberNavController(),
     modifier: Modifier
 ){
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Home.name,
+
+            modifier = Modifier.padding(isiRuang)){
+            composable (route= Navigasi.Formulirku.name){
+                FormIsian(
+                    OnSubmitBtnClick ={
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+
+        }
+    }
 
 }
 
