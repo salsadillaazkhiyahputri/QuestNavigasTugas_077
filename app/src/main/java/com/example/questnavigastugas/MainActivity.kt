856b86ead3ad08279a8188d348a6879ead7271f0
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-07 18:22:08 +0700

    menambahkan dataapp

--- app/src/main/java/com/example/questnavigastugas/MainActivity.kt
+++ app/src/main/java/com/example/questnavigastugas/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.questnavigastugas
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,34 +15,16 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.questnavigastugas.ui.theme.QuestNavigasTugasTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             QuestNavigasTugasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    DataApp(modifier = Modifier)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestNavigasTugasTheme {
-        Greeting("Android")
     }
 }
