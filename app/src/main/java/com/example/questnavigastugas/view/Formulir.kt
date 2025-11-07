5db19390e01cac975f1de118260cde62d34f242b
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-07 16:33:05 +0700

    menambahkan topapp bar untuk form pendaftaran

--- app/src/main/java/com/example/questnavigastugas/view/Formulir.kt
+++ app/src/main/java/com/example/questnavigastugas/view/Formulir.kt
@@ -1,6 +1,12 @@
 package com.example.questnavigastugas.view
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -8,6 +14,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
@@ -30,4 +39,27 @@ fun FormIsian(
 
     val OrangeHeader = Color(0xFFFF8A65)
     val OrangeSubmit = Color(0xFFFF7043)
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.padding(top = 50.dp),
+                title = {
+                    Text(
+                        text = "Formulir Pendaftaran",
+                        fontSize = 30.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = OrangeHeader
+                )
+            )
+        },
+        modifier = modifier.fillMaxSize()
+    ){
+
+    }
+
 }
