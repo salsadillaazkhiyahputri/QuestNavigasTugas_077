3df9be554fec15b34815129c420deea14a1eeac0
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-07 18:23:15 +0700

    memperbaiki eror pada navigasi

--- app/src/main/java/com/example/questnavigastugas/Navigasi.kt
+++ app/src/main/java/com/example/questnavigastugas/Navigasi.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.questnavigastugas.view.FormIsian
+import com.example.questnavigastugas.view.Home
 
-enum class Navigasi{
+enum class Navigasi {
+
     Home,
     Formulirku,
     Detail
@@ -18,33 +20,38 @@ enum class Navigasi{
 
 @Composable
 fun DataApp(
-    navController: NavHostController  = rememberNavController(),
+    navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
-    Scaffold { isiRuang ->
+    Scaffold { isiRuang->
         NavHost(
             navController = navController,
             startDestination = Navigasi.Home.name,
 
             modifier = Modifier.padding(isiRuang)){
-            composable (Navigasi.Formulirku.name){
-                FormIsian(
-                    OnSubmitBtnClick ={
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian (
+                    //pilihanJK = JenisK.map { id -> konteks.resources.getString}
+                    onSubmitBtnClick ={
                         navController.navigate(Navigasi.Detail.name)
                     }
                 )
             }
-            composable (Navigasi.Detail.name){
+            composable(route = Navigasi.Detail.name){
                 TampilData(
-                    OnBackClick = {
+                    onBackClick = {
                         cancelAndBackToFormulirku(navController)
                     },
+                    onHomeClick = {
+                        navController.navigate(Navigasi.Home.name)
+                    }
+                    //}
                 )
             }
-            composable(Navigasi.Home.name){
+            composable(route = Navigasi.Home.name) {
                 Home(
-                    OnNavigateToPendaftaran = {
-                        navController.navigate(route = Navigasi.Formulirku.name)
+                    onNavigateToPendaftaran = {
+                        navController.navigate(Navigasi.Formulirku.name)
                     }
                 )
             }
@@ -52,11 +59,15 @@ fun DataApp(
     }
 }
 
+@Composable
+fun TampilData(onBackClick: () -> Unit, onHomeClick: () -> Unit) {
+    TODO("Not yet implemented")
+}
+
+
 private fun cancelAndBackToFormulirku(
-    navController: NavHostController
+    navController:NavHostController
 ){
     navController.popBackStack(Navigasi.Formulirku.name,
         inclusive = false)
 }
-
-
